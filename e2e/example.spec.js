58d2f5b6d385319aa7f1dcd87fc8baa316e82792
Anderson-AI-Bank <andersonbankole1@gmail.com>
2024-07-23 14:04:26 +0100

    commiting changes to files install dependices for playwright

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -4,7 +4,7 @@ const { test, expect } = require('@playwright/test');
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substringTest.
+  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
@@ -14,6 +14,6 @@ test('get started link', async ({ page }) => {
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
