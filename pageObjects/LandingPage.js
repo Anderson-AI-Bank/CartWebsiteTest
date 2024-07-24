e2c26e9df9efafefe7bf18733368e93ad695c6a0
Anderson-AI-Bank <andersonbankole1@gmail.com>
2024-07-24 21:40:30 +0100

    Adding landing page objects

--- pageObjects/LandingPage.js
+++ pageObjects/LandingPage.js
@@ -1,13 +1,31 @@
-class LandingPage{
-    constructor(page){
-        this.product = page.locator('.inventory_list'); //(This is how you write an element with class attribute)
-        this.productText = page.locator('.inventory_item_name'); //(Text for all products on page)
-        this.burgerMenu = page.locator('[id*=react-burger-menu-btn]')
-        this.shoppingCardLink = page.locator('.shopping_cart_link')
-        this.AddtoCartBtn = paeg.locator('.btn btn_primary btn_small btn_inventory');
-    }
+class LandingPage {
+  constructor(page) {
+    this.products = page.locator(".inventory_list"); //(This is how you write an element with class attribute)
+    this.productsText = page.locator(".inventory_item_name"); //(Text for all products on page)
+    this.burgerMenu = page.locator("[id*=react-burger-menu-btn]");
+    this.shoppingCardLink = page.locator(".shopping_cart_link");
+    this.AddtoCartBtn = page.locator(
+      ".btn btn_primary btn_small btn_inventory",
+    );
+  }
 
-    async searchProduct(productName){
-        const titles = await this.productText
+  async searchProductAddCart(productName) {
+    const titles = await this.productsText.allTextContents();
+    console.log(titles);
+    const count = await this.products.count();
+    for (let i = 0; i < count; ++i) {
+      if (
+        (await this.products.nth(i).locator("bike").textContent()) ===
+        productName
+      ) {
+        //add to cart
+        await this.products.nth(i).locator("text= Add To Cart").click();
+        break;
+      }
     }
+  }
+  async navigateToCart() {
+    await this.shoppingCardLink.click();
+  }
 }
+module.exports = { LandingPage };
