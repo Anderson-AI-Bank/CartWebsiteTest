0f27fb76e3e4a514c2c9190a8688f7c1d7602fcd
Anderson-AI-Bank <andersonbankole1@gmail.com>
2024-07-25 17:54:37 +0100

    Adding LoginPage ts file

---
+++ pageObjects/LoginPage.ts
@@ -0,0 +1,35 @@
+import { Page, Locator } from '@playwright/test';
+
+class LoginPage {
+  private page: Page;
+  private userName: Locator;
+  private password: Locator;
+  private signInButton: Locator;
+  private pageLogo: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.userName = page.locator("[id*='username']");
+    this.password = page.locator("[id*='password']");
+    this.signInButton = page.locator("#login-button");
+    this.pageLogo = page.locator(
+      '//div[@class="login_logo" and text()="Swag Labs"]',
+    );
+  }
+
+  async goTo(): Promise<void> {
+    await this.page.goto("https://www.saucedemo.com/");
+  }
+
+  async enterLoginDetails(userName: string, password: string): Promise<void> {
+    await this.userName.fill(userName);
+
+    await this.password.fill(password);
+  }
+
+  async selectSignIn(): Promise<void> {
+    await this.signInButton.click();
+  }
+}
+
+export { LoginPage };
